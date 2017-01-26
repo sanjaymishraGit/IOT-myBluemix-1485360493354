adbcca1774d63d5b9e7cdf54024852ac0c0c08c0
sanjaymishraGit <sanjaymishra.mymail@gmail.com>
2017-01-26 09:47:01 +0530

    added CRC

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -31,6 +31,9 @@ var settings = module.exports = {
 
     // Add the bluemix-specific nodes in
     nodesDir: path.join(__dirname,"nodes"),
+    functionGlobalContext:{
+        CRC:require('CRC');
+    },
 
     // Blacklist the non-bluemix friendly nodes
     nodesExcludes:['66-mongodb.js','75-exec.js','35-arduino.js','36-rpi-gpio.js','25-serial.js','28-tail.js','50-file.js','31-tcpin.js','32-udp.js','23-watch.js'],
